2e3add2f5428334641e958ed90c34e604b008f74
Dmitriy Pushkov <ezze@ezze.org>
2018-10-01 22:07:47 +0300

    Dragged route list item is highlighted.

--- src/components/RouteList.js
+++ src/components/RouteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { Droppable } from 'react-beautiful-dnd';
 
 import RouteListItem from './RouteListItem';
@@ -11,14 +12,20 @@ class RouteList extends Component {
         const { points, removePoint } = this.props;
         return (
             <Droppable droppableId={DROPPABLE_ROUTE_LIST} direction="vertical">
-                {(provided, snapshot) => (
-                    <ul ref={provided.innerRef} {...provided.droppableProps} className="route-list">
-                        {points.map((point, i) => (
-                            <RouteListItem key={i} index={i} {...point} removePoint={removePoint} />
-                        ))}
-                        {provided.placeholder}
-                    </ul>
-                )}
+                {(provided, snapshot) => {
+                    const className = classNames({
+                        'route-list': true,
+                        'route-list-dragging-over': snapshot.isDraggingOver
+                    });
+                    return (
+                        <ul ref={provided.innerRef} {...provided.droppableProps} className={className}>
+                            {points.map((point, i) => (
+                                <RouteListItem key={i} index={i} {...point} removePoint={removePoint} />
+                            ))}
+                            {provided.placeholder}
+                        </ul>
+                    );
+                }}
             </Droppable>
         );
     }
--- src/components/RouteListItem.js
+++ src/components/RouteListItem.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { Draggable } from 'react-beautiful-dnd';
 
 class RouteListItem extends Component {
@@ -12,23 +13,26 @@ class RouteListItem extends Component {
         const { index, id, name } = this.props;
         return (
             <Draggable draggableId={`draggable-route-list-item-${id}`} index={index}>
-                {(provided, snapshot) => (
-                    <li
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        className="route-list-item"
-                        style={provided.draggableProps.style}
-                    >
-                        <span className="route-list-item-name">{name}</span>
-                        <span
-                            className="route-list-item-remove"
-                            onClick={this.onRemoveClick}
-                            title="Удалить">
-                            &times;
-                        </span>
-                    </li>
-                )}
+                {(provided, snapshot) => {
+                    const className = classNames({
+                        'route-list-item': true,
+                        'route-list-item-dragging': snapshot.isDragging
+                    });
+                    return (
+                        <li
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            className={className}
+                            style={provided.draggableProps.style}
+                        >
+                            <span className="route-list-item-name">{name}</span>
+                            <span className="route-list-item-remove" onClick={this.onRemoveClick} title="Удалить">
+                                &times;
+                            </span>
+                        </li>
+                    );
+                }}
             </Draggable>
         );
     }

cbd5d0096e0547129a774df8bd1e15ad3022d133
Dmitriy Pushkov <ezze@ezze.org>
2019-09-23 22:20:19 +0300

    Code style of ymaps mock is fixed.

--- __mocks__/ymaps.js
+++ __mocks__/ymaps.js
@@ -1,68 +1,68 @@
 import {
-    defaultMapLatitude,
-    defaultMapLongitude,
-    defaultMapZoom
+  defaultMapLatitude,
+  defaultMapLongitude,
+  defaultMapZoom
 } from '../src/constants';
 
 const ymaps = {
-    ready: jest.fn(cb => setTimeout(cb, 200)),
-    Map: jest.fn((container, options) => {
-        options = options || {};
+  ready: jest.fn(cb => setTimeout(cb, 200)),
+  Map: jest.fn((container, options) => {
+    options = options || {};
 
-        const {
-            center = [defaultMapLatitude, defaultMapLongitude],
-            zoom = defaultMapZoom
-        } = options;
+    const {
+      center = [defaultMapLatitude, defaultMapLongitude],
+      zoom = defaultMapZoom
+    } = options;
 
-        return {
-            controls: {
-                add: jest.fn()
-            },
-            events: createEvents(),
-            geoObjects: {
-                add: jest.fn(),
-                remove: jest.fn()
-            },
-            getCenter: jest.fn(() => center),
-            getZoom: jest.fn(() => zoom),
-            destroy: jest.fn()
-        };
-    }),
-    Polyline: jest.fn(() => {
-        return {
-            geometry: {
-                getCoordinates: jest.fn(),
-                setCoordinates: jest.fn()
-            }
-        };
-    }),
-    Placemark: jest.fn(() => {
-        return {
-            events: createEvents(),
-            geometry: {
-                getCoordinates: jest.fn(),
-                setCoordinates: jest.fn()
-            }
-        };
-    })
+    return {
+      controls: {
+        add: jest.fn()
+      },
+      events: createEvents(),
+      geoObjects: {
+        add: jest.fn(),
+        remove: jest.fn()
+      },
+      getCenter: jest.fn(() => center),
+      getZoom: jest.fn(() => zoom),
+      destroy: jest.fn()
+    };
+  }),
+  Polyline: jest.fn(() => {
+    return {
+      geometry: {
+        getCoordinates: jest.fn(),
+        setCoordinates: jest.fn()
+      }
+    };
+  }),
+  Placemark: jest.fn(() => {
+    return {
+      events: createEvents(),
+      geometry: {
+        getCoordinates: jest.fn(),
+        setCoordinates: jest.fn()
+      }
+    };
+  })
 };
 
 function createEvents() {
-    const _listeners = {};
-    return {
-        add: jest.fn((event, listener) => {
-            if (!Array.isArray(_listeners[event])) {
-                _listeners[event] = [];
-            }
-            _listeners[event].push(listener);
-        }),
-        trigger(event) {
-            const listeners = _listeners[event] || [];
-            for (let i = 0; i < listeners.length; i++) {
-                listeners[i]();
-            }
-        }
-    };
+  const listeners = {};
+  return {
+    add: jest.fn((event, listener) => {
+      if (!Array.isArray(listeners[event])) {
+        listeners[event] = [];
+      }
+      listeners[event].push(listener);
+    }),
+    trigger(event) {
+      const eventListeners = listeners[event] || [];
+      for (let i = 0; i < eventListeners.length; i++) {
+        eventListeners[i]();
+      }
+    }
+  };
 }
 
 export default ymaps;
